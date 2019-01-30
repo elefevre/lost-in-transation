ed50a5375487122d17a6812da00405598127c178
elefevre <eric@ericlefevre.net>
2019-01-30 22:45:13 +0100

    Display the top 25 words

--- java_plain_old/src/org/wordcount/Counter.java
+++ java_plain_old/src/org/wordcount/Counter.java
@@ -1,9 +1,58 @@
 package org.wordcount;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 public class Counter {
+
+    public static void main(String[] argv) throws IOException {
+        String filename = argv[0];
+
+        String text;
+
+        BufferedReader br = new BufferedReader(new FileReader(filename));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(" ");
+                line = br.readLine();
+            }
+            text = sb.toString();
+        } finally {
+            br.close();
+        }
+
+        Map<String, Integer> results = new Counter().countOccurrences(text);
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(results.entrySet());
+
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+
+        int count = 0;
+        for (Map.Entry<String, Integer> word : list) {
+            System.out.println(word.getKey() + ": " + word.getValue());
+            count++;
+            if (count > 25) {
+                break;
+            }
+        }
+
+    }
 
     public Map<String, Integer> countOccurrences(String text) {
         HashMap<String, Integer> map = new HashMap<>();
