d4366e60a49144713fe9ab22732e0a92e6d9940d
Fabrice <fabrice.vaillant@centraliens.net>
2019-02-20 08:11:37 +0100

    [ocaml] Finishes a correct versions

--- ocaml/freq.ml
+++ ocaml/freq.ml
@@ -1,18 +1,25 @@
 open Base
 open Stdio
 
-let build_counts () =
-  In_channel.fold_lines In_channel.stdin ~init:[] ~f:(fun counts line ->
-    let count =
-      match List.Assoc.find ~equal:String.equal counts line with
-      | None -> 0
-      | Some x -> x
-    in
-    List.Assoc.add ~equal:String.equal counts line (count + 1)
+let build_counts filename =
+  let ic = In_channel.create filename in
+  let counts = (Map.empty (module String)) in
+  In_channel.fold_lines ic ~init:counts ~f:(fun counts line ->
+    let words = String.split ~on:' ' line in
+    List.fold words ~init:counts ~f:(fun counts word ->
+      let count =
+        match Map.find counts word with
+        | None -> 0
+        | Some x -> x
+      in
+      Map.set counts ~key:word ~data:(count + 1)
+    )
   )
 
 let () =
-  build_counts ()
+  let filename = Sys.argv.(1) in
+  build_counts (filename)
+  |> Map.fold ~f:(fun ~key:k ~data:v l -> (k,v)::l) ~init:[]
   |> List.sort ~compare:(fun (_,x) (_,y) -> Int.descending x y)
-  |> (fun l -> List.take l 10)
-  |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
+  |> (fun l -> List.take l 25)
+  |> List.iter ~f:(fun (word,count) -> printf "%s: %3d\n" word count)
